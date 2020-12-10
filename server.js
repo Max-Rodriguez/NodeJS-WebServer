8799d11819cc78b6ff9c7ab67dd27ad0bedb0df7
Max Rodriguez <maxrcoppola@gmail.com>
2020-12-09 18:30:46 -0700

    Updated URL Routing

--- server.js
+++ server.js
@@ -1,40 +1,82 @@
-// ----- Load Node Modules -----
+// ----- Load Node Modules ----- //
 
 const http = require('http');
-const request_handler = require('./requestHandler');
+const url = require('url');
+const ResourceHandler = require('./resourceHandler');
 
 console.log("Imported Required Modules!")
 
 
-// ----- Create Server Listener Function-----
+// ----- Create Server Listener Function----- //
 
 const server = http.createServer((request, response) => {
 
-    var url = request.url;
+    // Request Variables
+    const req_url = url.parse(request.url, true);
+    const req_headers = request.headers;
+    const req_query = req_url.query;
+    const req_method = request.method.toLowerCase();
 
-    if (url === "/") {
-        
-        response = request_handler.homepage(response);
-        response.end();
+    // Preparing URL Parsed
+    let path = req_url.pathname;
+    path = path.replace(/^\/+|\/+$/g, ""); // Removing '/' if at end of URL.
 
-        console.log("Served Response; Code 200");
+    // ----- Routing Requests ----- //
 
-    }
+    request.on("data", () => {
 
-    else {
-        
-        response = request_handler.code_404(response);
-        response.end();
+        console.log("Additional Data Received From Request..")
+        // This listener is needed for "end" listener to work.
 
-        console.log("Served Response; Code 404");
+    })
 
-    }
+    request.on("end", () => {
+
+        let route_function = typeof routes[path] !== "undefined" ? routes[path] : routes["___404___"];
+        // Search for path on routes dictionary, else get 404 function.
+
+        let data = {
+
+            path: path,
+            queryString: req_query,
+            headers: req_headers,
+            method: req_method
+
+        }
+
+        route_function(data, response);
+
+        console.log("Served Response for route [ /" + path + " ]")
+
+    })
+
 });
 
+// ----- Routing List ----- //
 
-// ----- Initiate Server -----
+const routes = {
+
+    "": (data, response) => {
+
+        response = ResourceHandler.homepage(response);
+        response.end();
+
+    },
+
+    "___404___": (data, response) => {
+
+        response = ResourceHandler.code_404(response);
+        response.end();
+
+    }
+
+}
+
+
+// ----- Initiate Server ----- //
 
 const port = 3000;
-server.listen(port);
 
-console.log("Web Server Listening On Port " + port + "..");
+server.listen(port, () => {
+    console.log("Web Server Listening On Port " + port + "..");
+});
